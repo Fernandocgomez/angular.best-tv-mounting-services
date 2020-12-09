9c41ede01267db8f992e594bb8abb8009392d15d
fernandocgomez <fernandocgomez@live.com>
2020-12-08 23:58:51 -0500

    DEC 8 tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppConfig } from './models/app-config.models';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'best-tv-mounting-services';
+  constructor(private appConfig: AppConfig) {
+    console.log("fernando", this.appConfig.API_BASE_URL)
+  }
 }
---
+++ src/app/services/config.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { AppConfig } from "../models/app-config.models";
+import { HttpClient } from "@angular/common/http"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService extends AppConfig {
+
+  constructor(private http: HttpClient) { 
+    super();
+  }
+
+  // This function needs to return a promise
+  load() {
+    return this.http.get<AppConfig>('../../assets/config/config.json')
+      .toPromise()
+      .then((data:AppConfig) => {
+        this.API_BASE_URL = data.API_BASE_URL
+      })
+      .catch(() => {
+        console.error('Could not load configuration');
+      });
+  }
+}
