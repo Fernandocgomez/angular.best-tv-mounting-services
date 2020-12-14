f40164cf614bb2ce9cdeaa03cbaed1770cc64154
fernandocgomez <fernandocgomez@live.com>
2020-12-13 19:58:48 -0500

    fixed all css bugs && set an standar for media queries && devided components comments

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -8,15 +8,18 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent implements OnInit {
-  // Private methods
+  // Private properties
   private zipCodeRegExp: RegExp = /^\d{5}(?:[-\s]\d{4})?$/;
   private ZipCodeRequired: boolean = true;
 
-  // Public methods
+  // Public properties
   public heading: string = 'TV MOUNTING INSTALLATION';
   public subHeading: string = 'Compare prices & installation costs. Find best deals and compare for free right now!';
   public btnText: string = 'Get Started';
   public inputDisclaimer: string = 'Top Texas Tv Mounting Contractors';
+  public video: string = "../../../assets/hero-video.mp4";
+  public zipCodePlaceHolder: string= "Zip Code";
+  // Reactive forms
   public form = new FormGroup({
     zipCode: new FormControl('', [
       this.ZipCodeRequired ? Validators.required : Validators.nullValidator,
